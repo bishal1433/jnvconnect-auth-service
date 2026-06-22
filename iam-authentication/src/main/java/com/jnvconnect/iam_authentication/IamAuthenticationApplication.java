e04deaf2bf9765c05d0ad24aab727a7b98daec28
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-06-23 00:00:54 +0530

    chore: initial project setup

---
+++ iam-authentication/src/main/java/com/jnvconnect/iam_authentication/IamAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.jnvconnect.iam_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamAuthenticationApplication.class, args);
+	}
+
+}
---
+++ iam-authentication/src/test/java/com/jnvconnect/iam_authentication/IamAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnvconnect.iam_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
